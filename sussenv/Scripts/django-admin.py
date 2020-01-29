88402c6ab97e736dd5eb9e97345c3577aaafc026
Ahmed Marzouk <ahmed.marzouk@greenfinch.ie>
2020-01-29 10:12:43 +0000

    ignore .vscode and the venv dir
    Co-Authored-By: Ahmed Marzouk <marzoukali@users.noreply.github.com>

--- sussenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\10\desktop\stormra\lap\pythonstuff\suss\sussenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
